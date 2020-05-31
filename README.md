4d601e63f7c81d3c6b81af9d2dec635f7b017f64
Erwan-ui <62938950+Erwan-ui@users.noreply.github.com>
2020-05-31 13:23:05 +0200

    french portfolio

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# website-french
+Portfolio
---
+++ webpack.dev.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require('./webpack.common');
+const merge = require('webpack-merge');
+const path = require('path');
+
+module.exports = merge(common, {
+  mode: 'development',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      _favicon: './src/assets/favicon.png',
+      get favicon() {
+        return this._favicon;
+      },
+      set favicon(value) {
+        this._favicon = value;
+      },
+    })
+ 
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', //3. Inject styles into DOM
+          'css-loader', //2. Turns css into commonjs
+          'sass-loader'
+        ] //1. Turns sass into css
+      }
+    ]
+  }
+});
